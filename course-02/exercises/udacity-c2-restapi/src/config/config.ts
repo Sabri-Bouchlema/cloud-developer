cdf50071deb7f4f6332b0fcd01b4dd462894abb7
Sabri Bouchlema <bouchlema.sabri@gmail.com>
2020-12-19 18:15:00 +0100

    use env variables to run project

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "username": process.env.RDS_UDAGRAM_USERNAME,
+    "password": process.env.RDS_UDAGRAM_PASSWORD,
+    "database": process.env.RDS_UDAGRAM_DATABASE,
+    "host": process.env.RDS_UDAGRAM_HOST,
+    "dialect": process.env.DIALECT,
+    "aws_region": process.env.AWS_UDAGRAM_REGION,
+    "aws_profile": process.env.AWS_UDAGRAM_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "prod": {
     "username": "",
